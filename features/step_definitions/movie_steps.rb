af3cb2340c6f28ac8feba017209186df351e0b5c
Raja Rao Tadimeti <t_rajarao@yahoo.com>
2012-03-22 03:12:02 -0700

    Added step definition for "Given I select the following ratings: .."

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -25,4 +25,7 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s+/).each do |rating|
+    When %Q{I #{uncheck}check #{rating}}
+  end
 end
